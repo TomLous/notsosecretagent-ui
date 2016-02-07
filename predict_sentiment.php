60be3eb651d96eae1e711e0144369e7f1500d8a8
Tom Lous <tomlous@gmail.com>
2016-02-07 10:04:53 +0100

    try

--- predict_sentiment.php
+++ predict_sentiment.php
@@ -6,9 +6,10 @@
  * Datetime:     07/02/16 09:35
  */
 
+$path = "/var/www/notsosecretagent-r/";
 $text = "ISIS, IS, the Islamic State, ISIL -- the international community can't seem to decide what to call the Islamic extremists who been terrorizing the Middle East, and now the French government has announced it will use yet another name for them, wh..";
 
-$cmd = "/usr/bin/Rscript /var/www/notsosecretagent-r/sentimentPredictionNaive.R \"$text\" 2>&1";
+$cmd = "/usr/bin/Rscript {$path}sentimentPredictionNaive.R \"$text\" \"$path\" 2>&1";
 echo $cmd;
 $e = exec($cmd);
 var_dump($e);
